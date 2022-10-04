f01b5a0c23669f850a419a4ccb93262873c81d15
Mariam-Ali <ali.mariam.mwanasiti@gmail.com>
2022-10-04 16:40:30 -0400

    displaying associated data

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,9 @@
 class ItemsController < ApplicationController
+
+    def index
+        items = Item.all
+        render json: items, except: [:created_at, :updated_at, :user_id], 
+        include: {user: {except: [:created_at, :updated_at]}}
+    end
+
 end
